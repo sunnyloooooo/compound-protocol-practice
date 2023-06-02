58f6dbfce7ff541099da9709524f763f24a620e4
sunny <sunny.lo@tkspring.com>
2023-06-02 11:48:43 +0800

    fix: lib import

--- script/CompoundDeployScript.sol
+++ script/CompoundDeployScript.sol
@@ -1,28 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {CErc20Delegator} from "./compound-protocal/CErc20Delegator.sol";
-import {Erc20} from 'openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
-import {Unitroller} from "./compound-protocal/Unitroller.sol";
-import {SimplePriceOracle} from "./compound-protocal/SimplePriceOracle.sol";
-import {WhitePaperInterestRateModel} from "./compound-protocal/WhitePaperInterestRateModel.sol";
+import "forge-std/Script.sol";
+import {CErc20Delegator} from "compound-protocal/contracts/CErc20Delegator.sol";
+import {Erc20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Unitroller} from "compound-protocal/contracts/Unitroller.sol";
+import {SimplePriceOracle} from "compound-protocal/contracts/SimplePriceOracle.sol";
+import {WhitePaperInterestRateModel} from "compound-protocal/contracts/WhitePaperInterestRateModel.sol";
 
 contract CompoundDeployScript {
     // Deploy CErc20Delegator, Unitroller, and related contracts
     function deployContracts() public {
         // Deploy underlying ERC20 token
         Erc20 underlyingToken = new Erc20("Underlying Token", "UNDERLYING", 18);
-        
+
         // Deploy SimplePriceOracle
         SimplePriceOracle priceOracle = new SimplePriceOracle();
-        
+
         // Deploy WhitePaperInterestRateModel
         WhitePaperInterestRateModel interestRateModel = new WhitePaperInterestRateModel();
         interestRateModel.init(0, 0, 0, 0); // Set borrow and supply rates to 0%
-        
+
         // Deploy Unitroller
         Unitroller unitroller = new Unitroller();
-        
+
         // Deploy CErc20Delegator
         // Deploy CErc20Delegator
         CErc20Delegator cToken = new CErc20Delegator(
@@ -34,11 +35,11 @@ contract CompoundDeployScript {
             "CTKN",
             18,
             payable(msg.sender),
-            address(0),  // Replace with your implementation contract address
+            address(0), // Replace with your implementation contract address
             ""
         );
-        
-       // Set cToken's implementation
+
+        // Set cToken's implementation
         cToken._setImplementation(address(implementation), false, "");
     }
 }
